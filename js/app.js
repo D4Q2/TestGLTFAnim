e323fbf34baed9a3091eea48932b127ba4e967cb
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2020-06-08 13:50:33 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -135,12 +135,10 @@ function render() {
 
 function onWindowResize() {
 
-  camera.aspect = container.clientWidth / container.clientHeight;
-
   // update the camera's frustum
   camera.updateProjectionMatrix();
 
-  renderer.setSize( container.clientWidth, container.clientHeight );
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
 
